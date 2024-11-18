c9aa0bf56de4cd25f3a8fd2c93c728d9716b7c6b
Leonardo Beluzi Loureiro <leonardobeluzi@gmail.com>
2024-11-18 19:30:24 -0300

    feat: create new methods

--- src/shortened-url/repository/shortened-url-repository.interface.ts
+++ src/shortened-url/repository/shortened-url-repository.interface.ts
@@ -1,8 +1,16 @@
 import { CreateShortenedUrlDto } from '../dto/create-shortened-url.dto';
+import { UpdateShortenedUrlDto } from '../dto/update-shortened-url.dto';
 import ShortenedUrl from '../entities/shortened-url.entity';
 
 export interface IShortenedUrlRepository {
   create(shortenedUrl: CreateShortenedUrlDto): Promise<ShortenedUrl>;
   findOneByUrl(url: string): Promise<ShortenedUrl | null>;
   updateHits(id: number): Promise<void>;
+  delete(shortUrl: string, userId: number): Promise<void>;
+  update(
+    shortUrl: string,
+    userId: number,
+    shortenedUrl: UpdateShortenedUrlDto,
+  ): Promise<ShortenedUrl | null>;
+  list(userId: number): Promise<ShortenedUrl[]>;
 }
--- src/shortened-url/repository/shortened-url.repository.ts
+++ src/shortened-url/repository/shortened-url.repository.ts
@@ -2,14 +2,14 @@ import { IShortenedUrlRepository } from './shortened-url-repository.interface';
 import ShortenedUrl from '../entities/shortened-url.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Url } from '../models/shortened-url.model';
-import { Repository } from 'typeorm';
+import { UpdateShortenedUrlDto } from '../dto/update-shortened-url.dto';
+import { IsNull, Repository } from 'typeorm';
 
 export class ShortenedUrlRepository implements IShortenedUrlRepository {
   constructor(
     @InjectRepository(Url)
     private urlRepository: Repository<Url>,
   ) {}
-
   async create(shortenedUrl: ShortenedUrl): Promise<ShortenedUrl> {
     const shortenedUrlEntity = this.urlRepository.create({
       ...shortenedUrl,
@@ -29,6 +29,7 @@ export class ShortenedUrlRepository implements IShortenedUrlRepository {
     const foundUrl = await this.urlRepository.findOne({
       where: {
         shortUrl: url,
+        deletedAt: IsNull(),
       },
     });
 
@@ -43,9 +44,63 @@ export class ShortenedUrlRepository implements IShortenedUrlRepository {
     await this.urlRepository.increment(
       {
         id,
+        deletedAt: IsNull(),
       },
       'hits',
       1,
     );
+  }
+
+  async delete(shortUrl: string, userId: number): Promise<void> {
+    await this.urlRepository.softDelete({
+      shortUrl,
+      deletedAt: IsNull(),
+      user: {
+        id: userId,
+      },
+    });
+  }
+
+  async update(
+    shortUrl: string,
+    userId: number,
+    shortenedUrl: UpdateShortenedUrlDto,
+  ): Promise<ShortenedUrl | null> {
+    const foundUrl = await this.urlRepository.findOne({
+      where: {
+        shortUrl,
+        deletedAt: IsNull(),
+        user: { id: userId },
+      },
+    });
+
+    if (!foundUrl) {
+      return null;
+    }
+
+    const shortenedUrlEntity = this.urlRepository.create({
+      ...foundUrl,
+      longUrl: shortenedUrl.longUrl,
+    });
+
+    const updatedShortenedUrl =
+      await this.urlRepository.save(shortenedUrlEntity);
+
+    return new ShortenedUrl({
+      ...updatedShortenedUrl,
+    });
+  }
+
+  async list(userId: number): Promise<ShortenedUrl[]> {
+    const foundUrls = await this.urlRepository.find({
+      where: {
+        deletedAt: IsNull(),
+        user: {
+          id: userId,
+        },
+      },
+    });
+
+    return foundUrls.map((foundUrl) => new ShortenedUrl(foundUrl));
   }
 }
