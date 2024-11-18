4bc845d710bea6f89114ff0cb295da8b8a5efc28
Leonardo Beluzi Loureiro <leonardobeluzi@gmail.com>
2024-11-17 23:10:03 -0300

    chore: create database config file

---
+++ src/commons/database/database.config.ts
@@ -0,0 +1,49 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { Env } from '../environment/env';
+
+interface ITypeOrmConfig {
+  [key: string]: TypeOrmModuleOptions;
+}
+
+const config: ITypeOrmConfig = {
+  local: {
+    type: 'sqlite',
+    database: Env.DATABASE_NAME,
+    entities: [],
+    synchronize: true,
+    autoLoadEntities: true,
+  },
+  test: {
+    type: 'sqlite',
+    database: ':memory:',
+    entities: [],
+    synchronize: true,
+    autoLoadEntities: true,
+  },
+  development: {
+    type: 'mysql',
+    host: Env.DATABASE_HOST,
+    port: Env.DATABASE_PORT,
+    username: Env.DATABASE_USERNAME,
+    password: Env.DATABASE_PASSWORD,
+    database: Env.DATABASE_NAME,
+    entities: [],
+    synchronize: true,
+    autoLoadEntities: true,
+  },
+  production: {
+    type: 'mysql',
+    host: Env.DATABASE_HOST,
+    port: Env.DATABASE_PORT,
+    username: Env.DATABASE_USERNAME,
+    password: Env.DATABASE_PASSWORD,
+    database: Env.DATABASE_NAME,
+    entities: [],
+    synchronize: true,
+    autoLoadEntities: true,
+  },
+};
+
+export const databaseConfigurationOptions: TypeOrmModuleOptions = {
+  ...config[Env.NODE_ENV],
+};
