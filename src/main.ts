92cb968143bf3d193a5082c8ee1beb2305a2af89
Leonardo Beluzi Loureiro <leonardobeluzi@gmail.com>
2024-11-18 11:36:31 -0300

    chore: add validation pipes and swagger to main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Env } from './commons/environment/env';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle(Env.SWAGGER_TITLE)
+    .setDescription(Env.SWAGGER_DESCRIPTION)
+    .setVersion(Env.APPLICATION_VERSION)
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(Env.SWAGGER_PATH, app, documentFactory);
+
+  const port = Env.APPLICATION_PORT;
+  await app
+    .listen(port)
+    .then(() => Logger.log(`API listening on port ${port}`))
+    .catch((error: any) => {
+      Logger.error(error);
+    });
 }
 bootstrap();
