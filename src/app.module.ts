01becfa3a4599710bce37a038231f1ea385b811e
Leonardo Beluzi Loureiro <leonardobeluzi@gmail.com>
2024-11-18 15:37:56 -0300

    chore: add auth dependency to module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { databaseConfigurationOptions } from './commons/database/database.config
 import { UrlShortenerModule } from './shortened-url/url-shortener.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({ ...databaseConfigurationOptions }),
     UrlShortenerModule,
     UserModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
