3bc22192d345eb1b4eba5cfb5a358f74e1ab1aa7
Leonardo Beluzi Loureiro <leonardobeluzi@gmail.com>
2024-11-18 13:57:25 -0300

    chore: add dependencies to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { databaseConfigurationOptions } from './commons/database/database.config';
 import { UrlShortenerModule } from './shortened-url/url-shortener.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({ ...databaseConfigurationOptions }),
     UrlShortenerModule,
+    UserModule,
   ],
   controllers: [],
   providers: [],
