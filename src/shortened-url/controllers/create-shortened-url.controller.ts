eb2f78ee18127b72d3fb740ad6def49de6514720
Leonardo Beluzi Loureiro <leonardobeluzi@gmail.com>
2024-11-18 16:46:03 -0300

    feat: read userId

--- src/shortened-url/controllers/create-shortened-url.controller.ts
+++ src/shortened-url/controllers/create-shortened-url.controller.ts
@@ -1,8 +1,18 @@
-import { Controller, HttpCode, HttpStatus, Post, Body } from '@nestjs/common';
-import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import {
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Body,
+  UseGuards,
+  Req,
+} from '@nestjs/common';
+import { ApiBearerAuth, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { CreateShortenedUrlDto } from '../dto/create-shortened-url.dto';
 import { CreateShortenedUrlService } from '../services/create-shortened-url.service';
 import { ShowShortenedUrlDto } from '../dto/show-shortened-url.dto';
+import { AuthGuard } from '../../commons/guard/auth.guard';
+import { Public } from '../../commons/decorator/public.decorator';
 
 @ApiTags('Url Shortener')
 @Controller('shorten-url')
@@ -13,10 +23,18 @@ export class CreateShortenedUrlController {
 
   @Post()
   @HttpCode(HttpStatus.OK)
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard)
+  @Public()
   @ApiOkResponse({ type: ShowShortenedUrlDto })
-  async create(@Body() createShortenedUrlDto: CreateShortenedUrlDto) {
+  async create(
+    @Req() request,
+    @Body() createShortenedUrlDto: CreateShortenedUrlDto,
+  ) {
+    const userId = request.user ? request.user.sub : null;
     const shortenedUrl = await this.createShortenedUrlService.execute(
       createShortenedUrlDto,
+      userId,
     );
 
     return new ShowShortenedUrlDto(shortenedUrl);
